515536e9d4c65a3eb16eb81ae5d3a235a45c2270
Andrew Cho + David Diehr <cho-andrew@hotmail.com + ddiehr@yahoo.com>
2016-02-17 16:10:00 -0800

    finalize project with web interface.

--- Palindrome/Modules/HomeModule.cs
+++ Palindrome/Modules/HomeModule.cs
@@ -12,6 +12,14 @@ namespace PalidromeProject
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+      Post["/output"] = _ => {
+        string output;
+        bool answer;
+        Palidrome newPalidrome = new Palidrome(Request.Form["wordInput"]);
+        answer = newPalidrome.IsPalidrome();
+        output = answer.ToString();
+        return View["output.cshtml", output];
+      };
     }
   }
 }
